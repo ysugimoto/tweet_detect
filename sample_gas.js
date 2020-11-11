0f0be8166d556424879f1ac075cb3ed1c2405fa8
ysugimoto <sugimoto@wnotes.net>
2020-11-12 04:36:58 +0900

    rewrite by modarn syntax

--- sample_gas.js
+++ sample_gas.js
@@ -1,166 +1,155 @@
-//参考にさせていただいたURL： https://qiita.com/nobu09/items/c940fc6e0d67ef1cbc85
-var slackWebhookUrl = 'https://hooks.slack.com/services/******/***********/**********';
+// Ref: https://qiita.com/nobu09/items/c940fc6e0d67ef1cbc85
+const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/******/***********/**********';
 
-var twitter_api_key = '****';
-var twitter_api_key_secret = '****';
+// Twitter related configurations
+const TWITTER_API_KEY = '';
+const TWITTER_API_SECRET = '';
+const TWITTER_OAUTH_ENDPOINT = 'https://api.twitter.com/oauth2/token';
+const TWITTER_SEARCH_ENDPOINT = 'https://api.twitter.com/1.1/search/tweets.json';
+const TWITTER_LINK_URL = 'https://twitter.com/i/web/status';
 
-function main(){
-  var check_range = new Date();
-  var check_range = check_range.setMinutes(check_range.getMinutes() - 15); //Tweet検索対象の分数を指定
-  
-  //スプレッドシートの情報を抽出
-  var ｓpreadSheetObject = SpreadsheetApp.getActiveSpreadsheet(); //現在アクティブなスプレッドシートを定義
-  //「SaaSリスト」シート
-  var list_sheetObject = ｓpreadSheetObject.getSheetByName('SaaSリスト'); //シート名を定義
-  var list_sheetArray = list_sheetObject.getDataRange().getValues(); // シートの情報を全て二次元配列に保管
-  //「検知文言」シート
-  var downlist_sheetObject = ｓpreadSheetObject.getSheetByName('検知文言'); //シート名を定義
-  var downlist_sheetArray = downlist_sheetObject.getDataRange().getValues(); // シートの情報を全て二次元配列に保管
-  //「無視文言」シート
-  var ignore_list_sheetObject = ｓpreadSheetObject.getSheetByName('無視文言'); //シート名を定義
-  var ignore_list_sheetArray = ignore_list_sheetObject.getDataRange().getValues(); // シートの情報を全て二次元配列に保管
-  
-  //「無視文言」シートから除外対象のTweet文言を抽出する
-  var ignore_regexp = return_ignore_regexp(ignore_list_sheetArray);
+// sheet names
+const SERVICE_SHEET_NAME = 'services';
+const FILTER_WORDS_SHEET_NAME = 'filter_words';
+const IGNORE_WORDS_SHEET_NAME = 'ignore_words';
 
-  //スプレッドシートからTwitterの検索ログを抽出
-  for(var i=1; i<list_sheetArray.length; i++){ //1行目はヘッダーなので無視する。
-    var result_array = []; //配列を初期化
-    var search_key_1 = list_sheetArray[i][0]; //「SaaSリスト」シートのA列から検索対処のSaaS名を抽出
-    var threshold = list_sheetArray[i][1]; //「SaaSリスト」シートのB列からしきい値を抽出
-    
-    //「SaaSリスト」シートから抽出した内容と「検知文言」シートの内容をmergeしてTwitterから検索
-    for(var j=0; j<downlist_sheetArray.length; j++){
-      var search_key_2 = downlist_sheetArray[j][0];
-      var result_array = searchTweetsApps(result_array, search_key_1, search_key_2, check_range, ignore_regexp);
-      Utilities.sleep(100);
+// slack post items
+const SLACK_POST_ITEMS = 10;
+
+const makeIgnoreRegex = ignoreWords => {
+  const words = ignoreWords.reduce((prev, next) => {
+    const [word] = next;
+    if (word === "" || word === undefined) {
+      return prev;
     }
+    prev.push(`.*${word}.*`);
+    return prev;
+  }, []);
 
-    //エラー対策
-    if ( result_array == undefined ) { result_array = ["NULL"] }; //ヒットしなかった時用
-    var bool_1 = ( search_key_1 != "" ) && ( search_key_1 != undefined ); //スプレッドシートのセルが空欄だった時用(空欄ならfalseにする)
-    var bool_2 = ( search_key_2 != "" ) && ( search_key_2 != undefined );  //スプレッドシートのセルが空欄だった時用(空欄ならfalseにする)
-    
-    //検索日時の範囲に該当したログが指定件数以上あればSlack通知(1件だけのtweetとかだと誤検知の可能性もあるので)
-    var postNum = 10; //Slack通知する上限メッセージ数
-    if ( result_array.length > (threshold-1) && bool_1 && bool_2){
-      var summary_text = "「" + search_key_1 + "」で" + result_array.length + "件ヒットしました。(直近の" + String(postNum) + "件のみSlack通知します)";
-      postSlack(summary_text);
-      for(var k=0; k<result_array.length; k++){
-        postSlack(result_array[k]);
-        if (k==(postNum-1)){ break; }
+  const wordString = words.join('|');
+  return wordString !== '' ? new RegExp(wordString) : null;
+};
+
+// Fertch Twitter access bearer token from oauth
+const fetchAccessToken = () => {
+  const blob = Utilities.newBlob(`${TWITTER_API_KEY}:${TWITTER_API_SECRET}`);
+  const credential = Utilities.base64Encode(blog.getBytes());
+
+  const authRequestOptions = {
+    method: 'post',
+    contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
+    headers: {
+      Authorization: `Basic ${credential}`
+    },
+    payload: {
+      grant_type: 'client_credentials'
+    }
+  };
+  const authResponse = UrlFetchApp.fetch(TWITTER_OAUTH_ENDPOINT, authRequestOptions);
+  const { access_token } = JSON.parse(authResponse);
+
+  return access_token;
+}
+
+// Search twitter
+const searchTwitter = ({
+  serviceName,
+  filterWord,
+  ignores,
+  range,
+  accessToken
+}) => {
+  if (serviceName === '' || filterWord === '') {
+    return [];
+  }
+  const searchRequestOptions = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  };
+  const query = encodeURIComponent(`${serviceName} ${filterWord} -RT lang:ja`);
+  const searchResponse = UrlFetchApp.fetch(
+    `${TWITTER_SEARCH_ENDPOINT}?q=${query}&lang=ja&result_type=recent&count=100`,
+    searchRequestOptions
+  );
+  const { statuses } = JSON.parse(searchResponse);
+
+  return statuses.reduce((prev, next) => {
+    const {
+      text,
+      id,
+      created_at,
+      user: {
+        name
       }
+    } = next;
+
+    const tweetDate = new Date(created_at);
+    const serviceNameRegex = new RegExp(`.*${serviceName}.*`, 'i');
+    const filterWordRegex = new RegExp(`.*${filterWord}.*`, 'i');
+
+    // Check condition is matched
+    // If ignore words are included in tweet, skip it
+    if (ignores && ignores.test(text.toLowerCase())) {
+      return prev;
     }
-  }
-}
-
-function searchTweetsApps(result_array, app_name, detect_word, check_range, ignore_regexp) {
-  //引数
-  //arg1: 条件一致したTweet文を格納しておく配列
-  //arg2: Twitter検索したいSaaS名
-  //arg3: arg1に加えてand条件で検索したいキーワード
-  //arg4: 投稿日時の検知範囲
-  //arg5: 無視文言の正規表現パターンを指定
-  
-  if ( app_name == "" || app_name == undefined ) { return };
-  if ( detect_word == "" || detect_word == undefined ) { return };
-  
-  // ①Twitter Bearerトークンの取得（検索APIの呼び出しに必要）
-  // POST oauth2/token  https://developer.twitter.com/en/docs/basics/authentication/api-reference/token
-  var blob = Utilities.newBlob(twitter_api_key + ':' + twitter_api_key_secret);
-  var credential = Utilities.base64Encode(blob.getBytes());
-
-  var formData = {
-    'grant_type': 'client_credentials'
-  };
-
-  var basic_auth_header = {
-    'Authorization': 'Basic ' + credential
-  };
-
-  var options = {
-    'method': 'post',
-    'contentType': 'application/x-www-form-urlencoded;charset=UTF-8',
-    'headers':  basic_auth_header,
-    'payload': formData,
-  };
-
-  var oauth2_response = UrlFetchApp.fetch('https://api.twitter.com/oauth2/token', options);
-  var bearer_token = JSON.parse(oauth2_response).access_token; 
-
-  // ②Twitter 検索APIの呼び出し 
-  var bearer_auth_header = {
-    'Authorization': 'Bearer ' + bearer_token
-  };
-  
-  //Twitter検索
-  //var search_keyword = app_name + " -RT lang:ja";
-  var search_keyword = app_name + " " + detect_word + " -RT lang:ja";
-  var search_response = UrlFetchApp.fetch(
-    'https://api.twitter.com/1.1/search/tweets.json?q=' + search_keyword + '&lang=ja&result_type=recent&count=100',
-    { 'headers': bearer_auth_header });
-  var result = JSON.parse(search_response);
-  
-  //var result2 = []; //検索日時の範囲に合致するやつを格納する箱
-  result.statuses.forEach(function(status) {
-    var tweet_text = status.text;
-    var tweet_date = new Date(status.created_at);
-    var user_name = status.user.name;
-    
-    //Tweet文に求めている文言が含まれているか確認
-    //検索用の正規表現作成しとく
-    var app_name_regexp = new RegExp(".*" + app_name.toLowerCase() + ".*");
-    var detect_word_regexp = new RegExp(".*" + detect_word.toLowerCase() + ".*");
-    //Tweet文を精査
-    var tweet_text_2 = tweet_text.toLowerCase(); //tweet文言を小文字に変換しとく。
-    var bool_1 = app_name_regexp.test(tweet_text_2);
-    var bool_2 = detect_word_regexp.test(tweet_text_2);
-    var bool_3 = ( ! ignore_regexp.test(tweet_text_2) );
-    
-    //精査結果にマッチ かつ 検知範囲にマッチするものを抽出。
-    if ( check_range < tweet_date && bool_1 && bool_2 && bool_3 ) {
-      var jst = Utilities.formatDate(new Date(tweet_date), "JST", "yyyy/MM/dd HH:MM")
-      var tweet_data = "*" + user_name + "(" + jst + ")" + "*" + "\n";
-      var tweet_data = tweet_data + "```" + tweet_text + "```";
-      result_array.push(tweet_data)
+    // If service name and filter word are included, and also tweet time is in range, add to stack
+    if (range < tweetDate && serviceNameRegex.test(text.toLowerCase()) && filterWordRegex.test(text.toLowerCase())) {
+      const jst = Utilities.formatDate(tweetDate, "JST", "yyyy/MM/dd HH:MM");
+      prev.push(`*${name}(${jst})*\n\`\`\`${text}\`\`\`\n${TWITTER_LINK_URL}/${id}`);
     }
+    return prev;
+  }, []);
+};
+
+// Slack notification
+const postToSlack = text => {
+  UrlFetchApp.fetch(SLACK_WEBHOOK_URL, {
+    method: 'post',
+    contentType: 'application/json',
+    payload: JSON.stringify({ text })
   });
-  
-  return result_array;
-}
+};
 
-//Slack投稿するだけの関数
-function postSlack(text){
-  var data = {
-    "text": text
-  }
-  var options = {
-    'method' : 'post',
-    'contentType': 'application/json',
-    'payload' : JSON.stringify(data)
-  };
-  UrlFetchApp.fetch(slackWebhookUrl, options);
-}
+// Bootstrap function
+const main = () => {
+  const now = new Date();
+  const checkRange = now.setMinutes(now.getMinutes() - 15);
 
-//正規表現オブジェクトに変換するだけの関数
-function return_ignore_regexp(ignore_list_sheetArray){
-  //「無視文言」リストに記載のある内容に.*を前後につける
-  var temp_array = []
-  for(var i=0; i<ignore_list_sheetArray.length; i++){
-    var ignore_word = ignore_list_sheetArray[i][0];
-    if ( ignore_word != "" && ignore_word != undefined ){
-      var ignore_word = ".*" + ignore_list_sheetArray[i][0] + ".*";
-      temp_array.push(ignore_word)
+  const sheet = SpreadsheetApp.getActiveSpreadsheet();
+  const services = sheet.getSheetByName(SERVICE_SHEET_NAME).getDataRange().getValues();
+  const filterWords = sheet.getSheetByName(FILTER_WORDS_SHEET_NAME).getDataRange().getValues();
+  const ignoreWords = sheet.getSheetByName(IGNORE_WORDS_SHEET_NAME).getDataRange().getValues();
+  const ignoreRegex = makeIgnoreRegex(ignoreWords);
+
+  // Firstly, fetch Twitter access token
+  const accessToken = fetchAccessToken();
+
+  // Skip first row and search on Twitter
+  services.slice(1).forEach(service => {
+    const results = [];
+    const [serviceName, threshold] = service;
+
+    filterWords.forEach(filterWord => {
+      const response = searchTwitter({
+        serviceName: serviceName || '',
+        filterWord: filterWord[0] || '',
+        ignores: ignoreRegex,
+        range: checkRange,
+        accessToken
+      });
+      response.forEach(resp => result.push(resp));
+      Utilities.sleep(100);
+    });
+
+    if (results.length < threshold) {
+      return;
     }
-  }
-  
-  //「|」で連結する。(空の場合はダミーの文字列を入れとく)
-  var temp_array_join = temp_array.join("|");
-  if ( temp_array_join == "" ){
-    var temp_array_join = "_N_U_L_L_";
-  }
-  
-  //正規表現オブジェクトにして返す
-  var regexp = new RegExp(temp_array_join);
-  return regexp
-}
+    postToSlack(
+      `「${serviceName}」について${results.length}件のTweetが見つかりました。サービスが落ちているかもしれません。直近${SLACK_POST_ITEMS}件のTweetを通知します。`
+    );
+    results
+      .slice(0, SLACK_POST_ITEMS)
+      .forEach(item => postToSlack(item));
+  });
+};
